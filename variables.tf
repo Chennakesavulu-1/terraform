07350a2fb9d6767792f22905974d1616b55fea85
chennakesava <chennakesavulukallagunta@gmail.com>
2024-03-30 21:43:51 +0530

    tfvars

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "ami_id" {
+  type = string #this is the data type
+  default = "ami-03265a0778a880afb" #this is default value
+}
+
+variable "instances" {
+  type    = map
+  default = {
+    MongoDB   = "t3.medium"
+    MySQL     = "t3.medium"
+    Redis     = "t2.micro"
+    RabbitMQ  = "t2.micro"
+    Catalogue = "t2.micro"
+    User      = "t2.micro"
+    Cart      = "t2.micro"
+    Shipping  = "t2.micro"
+    Payment   = "t2.micro"
+    Web       = "t2.micro"
+
+  }
+}
+
+variable "zone_id" {
+  default = "Z08775202F3FFU3VO817"
+}
+variable "domain" {
+  default = "chennadevops.online"
+}
+#Declare variable but not providing values
+variable "sg_name" {
+  type = string
+}
+variable "sg_cidr" {
+  default = ["0.0.0.0/0"]
+}
